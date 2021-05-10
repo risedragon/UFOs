fcfa7cd60ded73f3651c0a48afa405face22263d
Andrew Lu <lvmzcn@gmail.com>
2021-05-09 22:14:53 -0400

    fiter data

--- static/js/app.js
+++ static/js/app.js
@@ -30,4 +30,15 @@ buildTable = (data) => {
         );
     });
 };
+//buildTable(tableData);
+
+function handleClick() {
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+    if (date) {
+        filteredData = filteredData.filter(row=>row.datetime === date )
+    };
+    buildTable(filteredData);
+};
+d3.selectAll("#filter-btn").on("click", handleClick);
 buildTable(tableData);
